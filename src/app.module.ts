c3f7ceea95d897626b14bc232890b1795a675e98
Usuario <135558153+IsaacNw321@users.noreply.github.com>
2025-11-18 17:39:30 -0400

    add : throttler for limit request

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,37 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatsModule } from './chats/chats.module';
 import { MessagesModule } from './messages/messages.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ([
+        {
+          ttl: config.get<number>('THROTTLE_TTL') || 60000, 
+          limit: config.get<number>('THROTTLE_LIMIT') || 10,
+        },
+      ]),
+    }),
     UserModule, 
     PrismaModule, 
     AuthModule, 
     ChatsModule, 
     MessagesModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
