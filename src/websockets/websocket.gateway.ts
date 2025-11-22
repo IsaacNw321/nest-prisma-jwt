8527c3b35394469dd02ce10ef5c8c306d2cbacc5
Usuario <135558153+IsaacNw321@users.noreply.github.com>
2025-11-22 11:20:51 -0400

    add: trycatch to users,chat,messages services

--- src/websockets/websocket.gateway.ts
+++ src/websockets/websocket.gateway.ts
@@ -20,7 +20,6 @@ export class MyWebSocketGateway
   @SubscribeMessage('joinRoom')
   handleJoinRoom(client: Socket, data: { roomId: string }) {
     client.join(data.roomId);
-    console.log(`Client ${client.id} joined room: ${data.roomId}`);
   }
   @SubscribeMessage('sendMessage')
   async handleSendMessage(client: Socket, payload: any): Promise<void> {
